8a6d1ebb631968c48ba032b7658ebbc392113d08
SvetilnikST <svetilnik.stezhkina@gmail.com>
2017-11-20 20:49:55 +0300

    viewdealcommand

---
+++ src/main/java/me/svetilnik/agentstvo/servlet/controller/command/ViewDealCommand.java
@@ -0,0 +1,22 @@
+package me.svetilnik.agentstvo.servlet.controller.command;
+
+import me.svetilnik.agentstvo.servlet.controller.ActionCommand;
+import me.svetilnik.agentstvo.servlet.controller.PageURL;
+import me.svetilnik.dbService.DAO.Impl.ViewDealDaoImpl;
+import me.svetilnik.dbService.hibernate.model.dataSet.ViewdealEntity;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+public class ViewDealCommand implements ActionCommand {
+    @Override
+    public String execute(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        ViewDealDaoImpl viewdealDao = new ViewDealDaoImpl();
+        List<ViewdealEntity> viewdealEntities = viewdealDao.getAll(0,0);
+
+        req.setAttribute("viewdeal", viewdealEntities);
+        return PageURL.VIEWDEAL_PAGE;
+    }
+}
