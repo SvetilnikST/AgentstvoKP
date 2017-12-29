fc50b555bf8734a1c5b913c1d5a5a56aac23e5b6
SvetilnikST <svetilnik.stezhkina@gmail.com>
2017-12-29 23:03:16 +0300

    -изменен способ подключения к Базе данных -добавлены тесты на AuthorizationDao

---
+++ src/test/me/svetilnik/dbService/DAO/Impl/ClientsDaoImplTest.java
@@ -0,0 +1,28 @@
+package me.svetilnik.dbService.DAO.Impl;
+
+import me.svetilnik.dbService.hibernate.model.dataSet.ClientsEntity;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ClientsDaoImplTest {
+    @Test
+    public void insert() throws Exception {
+        ClientsDaoImpl clientsDao = new ClientsDaoImpl();
+        ClientsEntity clientsEntity = new ClientsEntity();
+        clientsEntity.setSnmClients("snmClients");
+        clientsEntity.setFloor("floor");
+        clientsEntity.setPassportClients("passportClients");
+        clientsEntity.setPhoneClients("phoneClients");
+        clientsDao.insert(clientsEntity);
+    }
+
+    @Test
+    public void delete() throws Exception {
+//        Long clientsId = Long.parseLong(req.getParameter("id"));
+//        ClientsDaoImpl dao = new ClientsDaoImpl();
+//        ClientsEntity entity =dao.getById(clientsId);
+//        dao.delete(entity);
+    }
+
+}
